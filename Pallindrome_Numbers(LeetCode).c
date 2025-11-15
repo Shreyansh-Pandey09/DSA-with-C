54745f649c872c2599015140797f04613820fab5
Shreyansh-Pandey09 <shreyansh.pandey3115@gmail.com>
2025-11-15 11:54:08 +0530

    Create Pallindrome_Numbers(LeetCode).c

---
+++ Pallindrome_Numbers(LeetCode).c
@@ -0,0 +1,46 @@
+/*
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+*/
+
+                                                //CODE HERE:->
+
+#include<stdio.h>
+int main()
+{
+    int x,inp;
+    printf("Enter number: ");
+    scanf("%d", &inp);
+    x = inp;
+     if(x<0)
+        printf("false");
+    int num = x;
+    int rev = 0;
+    while(x>0)
+    {
+        num = x % 10;
+        rev = rev*10 + num;
+        x = x/10;
+    }
+    if(inp == rev)
+        printf("true");
+    else if(inp!=rev && inp>0)   
+        printf("false");
+}
